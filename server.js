2487ffa04658baf6c202a6883a83ae625d3e15a2
febriadj <iamfebriadji@gmail.com>
2021-08-28 23:59:10 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const MidtransClient = require('midtrans-client');
+const routes = require('./routes/index');
 
 require('dotenv').config();
 require('./ngrok')();
@@ -8,6 +10,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Inisialisasi Midtrans Client Snap
+const ApiClient = new MidtransClient.Snap({
+  isProduction: false,
+  serverKey: process.env.SERVER_KEY,
+  clientKey: process.env.CLIENT_KEY,
+});
+
+// parsing ApiClient
+app.use(routes(ApiClient));
 
 app.listen(port);
 console.log(`Server running on port ${port}`);
